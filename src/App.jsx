66b34e2b7f508734ab2d35d34d0080df09d9335d
prince475 <danielmwai475@gmail.com>
2023-06-03 23:37:18 +0300

    Removing Unnecessary use of state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import Header from './components/Header'
 import Meme from './components/Meme'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
